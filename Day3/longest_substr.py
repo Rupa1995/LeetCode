ece7076aa6f5cab6615eb1a35bf89f93a3dd4c5d
Rupa1995 <30503476+Rupa1995@users.noreply.github.com>
2025-02-10 07:32:25 +0000

    longest substr

--- Day3/longest_substr.py
+++ Day3/longest_substr.py
@@ -2,6 +2,8 @@ class Solution:
     def lengthOfLongestSubstring(self, s: str) -> int:
         res=""
         max_len = 1
+        if len(s) <= 0:
+            return 0
         for i in range(len(s)):
             res = s[i]
             for j in range(i+1 , len(s)):
